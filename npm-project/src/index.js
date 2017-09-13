c00ac681d491a37b111fafaabdbb934249106b3b
Art <art_jr2000@yahoo.com>
2017-09-13 12:09:07 -0500

    Included modules using require -added:  -data  -index  -math

--- npm-project/src/index.js
+++ npm-project/src/index.js
@@ -1,9 +1,17 @@
 const $ = require("jquery");
 
+const data = require("./data");
+
+const math = require("./math");
+
+console.log(math.add(10, 98));
+
 
 const  hello = () => console.log("hello redwood from webpack!");
 
 hello();
+
+console.log(data.whichSideOfTheForce);
 
 $("body")
     .html("<h1>Hello from jQuery</h1>")
