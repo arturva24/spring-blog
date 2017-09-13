ce6870625e08f1a036df45431714409c5c590d41
Art <art_jr2000@yahoo.com>
2017-09-13 11:32:05 -0500

    Installed jQuery and modified

--- npm-project/src/index.js
+++ npm-project/src/index.js
@@ -1,3 +1,10 @@
- const  hello = () => console.log("hello redwood from webpack!");
+const $ = require("jquery");
+
+
+const  hello = () => console.log("hello redwood from webpack!");
 
 hello();
+
+$("body")
+    .html("<h1>Hello from jQuery</h1>")
+    .css("background-color", "gray");
