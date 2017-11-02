f4fe284d9a6138d135331f77d476282b0286fa0b
Art <art_jr2000@yahoo.com>
2017-11-02 10:48:34 -0500

    Hello world controller

---
+++ src/main/java/com/codeup/blog/controllers/HelloWorldController.java
@@ -0,0 +1,38 @@
+package com.codeup.blog.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello, World!!";
+    }
+
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String helloName(@PathVariable String name) {
+        return "Hello, " + name + "!!";
+    }
+
+
+    @ResponseBody
+    @GetMapping ("/hello/{firstName}/{lastName}")
+    //for each one use a @PathVariable
+    public String helloFullName (@PathVariable String firstName, @PathVariable String lastName){
+        return "<h1>Hello " + firstName + " " + lastName + "!!</h1>";
+    }
+
+    @ResponseBody
+    @GetMapping("/square/{number}")
+    public Integer square(@PathVariable Integer number){
+        return number * number;
+    }
+}
